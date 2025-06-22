e99881dfcc931808f99caddac31aa99745f45b90
sohanbuet22 <sohanrgc12@gamil.com>
2025-06-22 14:51:34 +0600

    22june afternoon

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,8 +4,6 @@ import Home from './route/Home';
 import CustomerLogin from './route/CustomerLogin';
 import CustomerRegistration from './route/CustomerRegistration'
 import LoginOption from './route/LoginOption'
-import SellerLogin from './route/SellerLogin';
-import SellerRegistration from './route/SellerRegistration'
 import ProductDetails from './route/ProductDetails'
 import CartProducts from './route/CartProducts';
 const App = () => {
@@ -17,8 +15,6 @@ const App = () => {
           <Route path="/CustomerLogin" element={<CustomerLogin/>}/>
           <Route path="/CustomerRegistration" element={<CustomerRegistration/>}/>
           <Route path="/LoginOption" element={<LoginOption/>}/>
-          <Route path="/SellerLogin" element={<SellerLogin/>}/>
-          <Route path="/SellerRegistration" element={<SellerRegistration/>}/>
           <Route path="/product/:id" element={<ProductDetails />} />
           <Route path="/CartItems" element={<CartProducts/>}/>
         </Routes>
